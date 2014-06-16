9fc418958031d2be814763f1ff80ecf304f851a1
Kasicass Tang <kasicass@gmail.com>
2014-06-16 18:23:07 +0800

    ogl, 01_Window

---
+++ src/ogl/01_Window/main.cpp
@@ -0,0 +1,24 @@
+#include <GL/freeglut.h>
+
+static void renderSceneCallback()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("01_Window");
+
+	glutDisplayFunc(renderSceneCallback);
+
+	glClearColor(1.f, 0.f, 0.f, 0.f);
+	glutMainLoop();	
+
+	return 0;
+}
+
